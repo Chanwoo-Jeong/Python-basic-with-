baf9f18fd4021c37274ad50589aff283fdf4ff4a
Chanwoo-Jeong <lordroad38@naver.com>
2022-06-29 20:12:09 +0900

    boolean

---
+++ boolean.py
@@ -0,0 +1,21 @@
+#python
+
+#number
+print(1)
+#string
+print('Hello world')
+#Boolean
+print(True) #참
+print(False) #거짓
+#Expression
+print(1+1)
+print('Hello'+'World') #Hello World
+#Comparison operator
+print(1==1) #ture
+print(1<2) #ture
+print(2<1) #false
+print(2!=1) #True
+print('world' in 'hello world')#True
+
+import os.path
+print(os.path.exists('boolean.py'))#True
