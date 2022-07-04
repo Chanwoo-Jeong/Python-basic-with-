433d11eaf58b1455d103f8a810feec288c1a512d
Chanwoo-Jeong <lordroad38@naver.com>
2022-07-04 23:19:58 +0900

    python list,for study

---
+++ conditional.py
@@ -0,0 +1,32 @@
+user_id = input('id?')
+user_pwd = input('passworld?')
+
+# if user_id == 'chanwoo':
+#     print('Hello chanwoo')
+# else:
+#     print('u r not chanwoo')
+
+# if user_pwd == '1111':
+#     print('Hello world')
+# else:
+#     print('who are you?')
+
+'''
+if user_id == 'chanwoo':
+    if user_pwd == '1111':
+        print('Hello world')
+    else:
+        print('who are you?')
+else:
+    print('who are you not name?')
+'''
+
+if user_id == 'chanwoo' and user_pwd == '1111':
+    print('Hello master')
+elif user_id == 'minwoo' and user_pwd == '2222':
+    print('Hello minwoo')
+else:
+    print('who are you not name?')
+
+
+ 
